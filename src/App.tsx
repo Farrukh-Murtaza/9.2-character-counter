24cc22654dae61f2d3e56b4f71d75f2705673fee
Farrukh-Murtaza <farrukhmurtaza1@gmail.com>
2026-08-27 15:25:59 -0400

    feat: Setup the project with the necessary files and tested the tailwind working:success.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="bg-slate-600">Hello World</h1>
+    </div>
+  )
+
+
+}
+
+export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface TextInputProps {
+    onTextChange: (text: string) => void;
+    placeholder?: string;
+    initialValue?: string;
+}
+
+export interface TextInputProps {
+    onTextChange: (text: string) => void;
+    placeholder?: string;
+    initialValue?: string;
+}
+
+export interface TextInputProps {
+    onTextChange: (text: string) => void;
+    placeholder?: string;
+    initialValue?: string;
+}
